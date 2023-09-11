5b7771334699d52d09918a39b36b81f7da115fcf
thiencntt <nguyenchithien@gmail.com>
2023-09-11 16:38:45 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace CSharp_Bai9_SystemInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblRam = new System.Windows.Forms.Label();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.lblHDD = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(179, 82);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(261, 46);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Tên máy tính";
+            // 
+            // lblRam
+            // 
+            this.lblRam.AutoSize = true;
+            this.lblRam.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRam.Location = new System.Drawing.Point(179, 146);
+            this.lblRam.Name = "lblRam";
+            this.lblRam.Size = new System.Drawing.Size(112, 46);
+            this.lblRam.TabIndex = 1;
+            this.lblRam.Text = "RAM";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(525, 341);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(263, 76);
+            this.btnThoat.TabIndex = 2;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Coral;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(302, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(737, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TIỆN ÍCH LẤY THÔNG TIN MÁY TÍNH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Coral;
+            this.label2.Location = new System.Drawing.Point(33, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 46);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Coral;
+            this.label3.Location = new System.Drawing.Point(61, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 46);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "RAM";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Coral;
+            this.label4.Location = new System.Drawing.Point(65, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 46);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "CPU";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Coral;
+            this.label5.Location = new System.Drawing.Point(63, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 46);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "HDD";
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPU.Location = new System.Drawing.Point(179, 210);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(108, 46);
+            this.lblCPU.TabIndex = 8;
+            this.lblCPU.Text = "CPU";
+            // 
+            // lblHDD
+            // 
+            this.lblHDD.AutoSize = true;
+            this.lblHDD.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHDD.Location = new System.Drawing.Point(179, 274);
+            this.lblHDD.Name = "lblHDD";
+            this.lblHDD.Size = new System.Drawing.Size(110, 46);
+            this.lblHDD.TabIndex = 9;
+            this.lblHDD.Text = "HDD";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 441);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblHDD);
+            this.Controls.Add(this.lblCPU);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.lblRam);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblRam;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblCPU;
+        private System.Windows.Forms.Label lblHDD;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Management.Instrumentation;
+
+namespace CSharp_Bai9_SystemInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            long DungLuongRAM = 0;
+            // Tên máy tính
+            lblName.Text = Environment.MachineName.ToString();
+            // Lấy dung lượng bộ nhớ RAM
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher
+            ("SELECT * FROM Win32_Computersystem");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                DungLuongRAM = long.Parse(obj["TotalPhysicalMemory"].ToString());//Byte
+                DungLuongRAM = DungLuongRAM / (1024 * 1024 * 1024); // đổi sang GB
+            }
+            lblRam.Text = DungLuongRAM.ToString() + " GB";
+
+            // Tên CPU
+            string sCPUName = "";
+            ManagementObjectSearcher searcher2 = new ManagementObjectSearcher
+            ("SELECT * FROM Win32_Processor");
+            foreach (ManagementObject obj in searcher2.Get())
+            {
+                sCPUName = obj["Name"].ToString();
+            }
+            lblCPU.Text = sCPUName;
+            // Thông tin đĩa cứng (HDD)
+            string sHDDName = "";
+            float sHDDSize = 0;
+            ManagementObjectSearcher searcher3 = new ManagementObjectSearcher
+            ("SELECT * FROM Win32_DiskDrive");
+            foreach (ManagementObject obj in searcher3.Get())
+            {
+                foreach (PropertyData pd in obj.Properties)
+                {
+                    if (pd.Name == "Model")
+                    {
+                        sHDDName = pd.Value.ToString();
+                    }
+                    if (pd.Name == "Size")
+                    {
+                        string size = pd.Value.ToString(); // byte
+                        sHDDSize = float.Parse(size) / (1024 * 1024 * 1024); //GB
+                    }
+                }
+            }
+            lblHDD.Text = sHDDName + " - Size: " + sHDDSize.ToString() + " GB";
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
